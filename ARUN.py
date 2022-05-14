da8a770fd80c10d60514cade026c008e4c2ca1c2
APURBOROY00 <105502627+APURBOROY00@users.noreply.github.com>
2022-05-14 07:56:49 +0000

    Add files via upload

---
+++ ARUN.py
@@ -0,0 +1,10 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+from APRO import Token_token
+Token_token()
